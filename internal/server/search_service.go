f4505f28c8654511a2182f5828ca7dd9827a7b05
karim <karimdaw91@gmail.com>
2025-11-26 18:25:44 +0000

    feat: add .txt parsing for key word search

--- internal/server/search_service.go
+++ internal/server/search_service.go
@@ -28,20 +28,30 @@ func (s *searchService) Search(ctx context.Context, req *pb.SearchRequest) (*pb.
 	if err != nil {
 		return nil, fmt.Errorf("failed to read folder: %v", err)
 	}
+	// convert query to lower case for case insensitive search
+	searchQueryLower := strings.ToLower(searchQuery)
 
-	// just return the files for now
 	results := []string{}
 	for _, file := range files {
 		// check if string in query is in file name
-		// first conver to lower
-		searchQueryLower := strings.ToLower(searchQuery)
 		fileLower := strings.ToLower(file.Name())
 		if strings.Contains(fileLower, searchQueryLower) {
 			results = append(results, filepath.Join("tests/test_folder", file.Name()))
 		}
 	}
 
-	return &pb.SearchResponse{
-		Results: results,
-	}, nil
+	// if no matches were found in the file title, we parse the content of the file to find matches
+	if len(results) == 0 {
+
+		for _, file := range files {
+			content, err := os.ReadFile(filepath.Join("tests/test_folder", file.Name()))
+			if err != nil {
+				return nil, fmt.Errorf("failed to read file: %v", err)
+			}
+			if strings.Contains(strings.ToLower(string(content)), searchQueryLower) {
+				results = append(results, filepath.Join("tests/test_folder", file.Name()))
+			}
+		}
+	}
+	return &pb.SearchResponse{Results: results}, nil
 }
