9e420df28d95cca232e66fda26355c8818f628f9
karim-daw <karimdaw91@gmail.com>
2025-12-20 18:41:15 +0000

    ref: update model with image stuff

--- internal/db/models.go
+++ internal/db/models.go
@@ -3,13 +3,15 @@ package db
 import "time"
 
 type Document struct {
-	ID         string
-	Path       string
-	FileType   string
-	ModifiedAt time.Time
-	Size       int64
-	Metadata   any
-	Content    string
+	ID            string
+	Path          string
+	FileType      string
+	ModifiedAt    time.Time
+	Size          int64
+	TextMetadata  any
+	Content       string
+	ContentType   string // 'text' or 'image'
+	ImageMetaData any
 }
 
 type Embedding struct {
